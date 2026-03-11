d387ec9bec486a172085c280d471aca3a93b3d9e
Maruf Hossain <maruf.bshs@gmail.com>
2026-03-12 02:19:17 +0900

    added some minor fixes

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 # import firebase_admin
 # from firebase_admin import credentials
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
