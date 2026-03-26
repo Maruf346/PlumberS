6afc0d442ce87930bb192c281881721f07cadb27
Maruf Hossain <maruf.bshs@gmail.com>
2026-03-27 04:16:29 +0600

    added manual pagination

--- core/settings.py
+++ core/settings.py
@@ -237,7 +237,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'core.pagination.FlexiblePageNumberPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
